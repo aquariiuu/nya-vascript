9d0c335abc6d1ec3185560f5d2e5aeb5d0bbe840
selectgender <119613700+selectgender@users.noreply.github.com>
2025-01-30 20:48:08 -0800

    add support for more programming languages

--- nyavascript.js
+++ nyavascript.js
@@ -1,5 +1,13 @@
-if(code.match(/i\'m a good girl/gims)){
-  eval(code)
-}else{
-  eternal death
+eval set -- "$code"
+
+(= code "$1")
+
+parse :: char* -> void
+void parse(char *buf) {
+	match buf {
+		/i\'m a good girl/gims => pcall(eval, code),
+		_ => eternal death
+	}
 }
+
+f, err = parse()
